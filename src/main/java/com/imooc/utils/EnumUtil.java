5a6cc73a61747f3cc19f113f4c210bf5c30f2751
keith <jianlinlo1988@163.com>
2020-12-31 17:59:27 +0800

    seller-order fixed

--- src/main/java/com/imooc/utils/EnumUtil.java
+++ src/main/java/com/imooc/utils/EnumUtil.java
@@ -3,11 +3,14 @@ package com.imooc.utils;
 import com.imooc.enums.CodeEnum;
 
 /**
+ * 通用的枚举类，通过code返回枚举实例
  * Created by 廖师兄
  * 2017-07-16 18:36
  */
 public class EnumUtil {
-
+	
+	//<T extends CodeEnum>是对枚举范型T的说明，说明枚举类T是继承了CodeEnum
+	//Class<T> enumClass输入enum的class类文件
     public static <T extends CodeEnum> T getByCode(Integer code, Class<T> enumClass) {
         for (T each: enumClass.getEnumConstants()) {  //返回枚举数组
             if (code.equals(each.getCode())) {
